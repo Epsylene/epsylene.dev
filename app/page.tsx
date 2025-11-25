c7651c0ddd243631068f68baaa949a08d5620212
Epsylene <epsylene@gmail.com>
2025-11-25 12:45:37 +0100

    Link to projects in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center mb-[5%]">
-        <h1 className="text-4xl mb-12">
+        <h1 className="text-4xl mb-8">
           epsylene
         </h1>
+        <div className="space-x-12 text-xl text-gray-300 [&>a]:hover:underline">
+          <Link href="/projects">Projects</Link>
+          {/* <Link href="/blog">Blog</Link> */}
+        </div>
       </div>
     </div>
   );
